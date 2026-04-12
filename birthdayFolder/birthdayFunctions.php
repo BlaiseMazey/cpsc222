7ffea8c01ddb6aa6f546e4f5df5a8691df84c9e8
Blaise Mazey <bxm414@francis.edu>
2026-04-12 16:49:57 -0400

    Update birthdayFunctions.php

--- birthdayFolder/birthdayFunctions.php
+++ birthdayFolder/birthdayFunctions.php
@@ -1,1 +1,22 @@
-
+<?php
+function convertHour($hour,$ampm)
+	{
+	if($ampm == "PM")
+		{
+		$hour+=11;
+		}
+		return $hour;
+	} //kinda pointless, gives the military time of the taken hour, expects ampm to return a string
+	
+function printBirthdayNicely($hour, $min, $second, $month,$day,$year,$ampm)
+	{
+$hour = convertHour($hour,$ampm);
+	return  date("l F jS, y - g:ia",mktime($hour,$min,$second,$month,$day,$year)). \n;
+	} //should work properly
+	function printBirthdayISO($hour, $min, $second, $month,$day,$year,$ampm)
+		{
+		$hour = convertHour($hour,$ampm);
+	return  date("c",mktime($hour,$min,$second,$month,$day,$year)). \n;
+	
+		}
+?>
