24ebf22b427733e3dc2370db65b6a6c3ef8b65f2
Blaise Mazey <bxm414@francis.edu>
2026-04-12 16:51:24 -0400

    Update birthdayWebsite.php

--- birthdayFolder/birthdayWebsite.php
+++ birthdayFolder/birthdayWebsite.php
@@ -1,35 +1,35 @@
 <?php
-$months = array("January" -> 1, "Feburary" -> 2, "March" -> 3, "April" -> 4, "May"->5,"June"->6,"July"->7,"August"->8,"September"->9, "October"->10, "November"->11, "December"->12);
+require_once "birthdayfunctions.php";
+
+$months = array("January" ->1, "Feburary"->2, "March"->3, "April"->4, "May"->5,"June"->6,"July"->7,"August"->8,"September"->9, "October"->10, "November"->11,"December"->12);
 $month= $_POST['month'] ;
 $day= $_POST['day'] ;
 $year = $_POST['year'];
 $hour = $_POST['hour'];
 $minute = $_POST['min'];
 $dayOrNight = $_POST['ampm'];
-$buttonPresses = "";
-if($minute == 0)
-        $minute = '0';
-
+$buttonPressed = false;
 ?>
 <!DOCTYPE html>
 <html lang=en>
 
-        <head>
-                <title> Birthday Format Fun</title>
-                <meta charset="utf-8" />
-                <meta name = "viewport" content="width=device-width"/>
+	<head>
+ 		<title> Birthday Format Fun</title>
+               	<meta charset="utf-8" />
+       	        <meta name = "viewport" content="width=device-width"/>
 </head>
-        <body>
+	<body>
 <h1>Birthday Formater</h1>
-
+<?php if($buttonPressed == false)//check this line
+{ ?>
 <form method="post" action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF']); ?>" >
 <table border=1 >
 <tr>
-<th><b>Month    </b> </th>
-<th><b>Day      </b> </th>
-<th><b>Year     </b> </th>
-<th><b>Hour     </b> </th>
-<th><b>Minute   </b> </th> 
+<th><b>Month	</b> </th>
+<th><b>Day	</b> </th>
+<th><b>Year	</b> </th>
+<th><b>Hour	</b> </th>
+<th><b>Minute	</b> </th> 
 <th><b>AM/PM</b> </th>
 
 </tr>
@@ -68,7 +68,7 @@ echo "</th>";
 //hour
 echo "<th> ";
 echo "<select name= \"hour\" > ";
-for($lcv=1;$lcv<=12; $lcv++ )
+for($lcv=0;$lcv<=11; $lcv++ )
 echo "<option value = ".$lcv."> ".$lcv ."</option> ";
 
 echo"</select> ";
@@ -78,7 +78,7 @@ echo "</th>";
 //min
 echo "<th> ";
 echo "<select name= \"min\" > ";
-for($lcv=0;$lcv<=60; $lcv++ )
+for($lcv=0;$lcv<=59; $lcv++ )
 echo "<option value = ".$lcv."> ".$lcv ."</option> ";
 echo"</select> ";
 echo "</th>";
@@ -99,21 +99,24 @@ echo "</th>";
 <br/>
 
 <br/>
-        <input type ="submit" name="submit" value ="Click Here"/input>
+	<input type ="submit" name="submit" value ="Click Here"/input>
 </form>
-
+<?php } //should close out the thing
+	else
+	{	
+	$monthNumber = $month->$months;
+	echo "<h3>".printBirthdayNicely($hour, $min, $second, $monthNumber,$day,$year,$dayOrNight)."<h3>";
+	}?> 
 <?php
 //echo "<a href = \"birthday.php? " concat all variables we took in to do thit then do \"">ClickHere" /</a>;
 ?>
 
 <?php
-if($_SERVER['REQUEST_METHOD'=="POST" ]) //mktime()
+if($_SERVER['REQUEST_METHOD'=="POST" ]) 
 {
-echo "work plz ". $month." ".$day." ".$year. " ".$hour." ".$minute." ".$dayOrNight;
+echo "<h2>this works now </h2>";
+//echo "work plz ". $month." ".$day." ".$year. " ".$hour." ".$minute." ".$dayOrNight;
 }
  ?>
-        </body>
+	</body>
 </html>
-
-
-<table border=1 >
