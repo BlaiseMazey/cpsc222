2ccb289cb03f975c493fcf07a5216b63fd55ce9c
Blaise Mazey <bxm414@francis.edu>
2026-04-11 20:58:21 -0400

    Update birthdayWebsite.php

--- birthdayWebsite.php
+++ birthdayWebsite.php
@@ -1,5 +1,5 @@
 <?php
-$months = array("January" , "Feburary", "March", "April", "May","June","July","August","September", "October", "November", "December");
+$months = array("January" -> 1, "Feburary" -> 2, "March" -> 3, "April" -> 4, "May"->5,"June"->6,"July"->7,"August"->8,"September"->9, "October"->10, "November"->11, "December"->12);
 $month= $_POST['month'] ;
 $day= $_POST['day'] ;
 $year = $_POST['year'];
